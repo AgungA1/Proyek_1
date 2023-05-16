bea8e6f6b8e4c10bd49abcbc54c2fdfd4b99be69
Roziq <roziqmahbubi22@gmail.com>
2023-05-16 12:03:27 +0700

    generating model & controller(resource controller for CRUD), completing database migration

---
+++ database/migrations/2023_05_12_065959_create_barang_masuks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang_masuk', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_gudang');
+            $table->unsignedBigInteger('kode_barang');
+            $table->integer('kuantitas_barang');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang_masuk');
+    }
+};
---
+++ database/migrations/2023_05_16_003832_create_staf_gudangs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staf_gudang', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_gudang');
+            $table->string('username_staf');
+            $table->string('nama_staf');
+            $table->string('email_staf')->unique();
+            $table->string('no_telp_staf');
+            $table->string('avatar_staf');
+            $table->string('password_staf');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staf_gudang');
+    }
+};
