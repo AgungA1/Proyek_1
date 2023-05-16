53d28662579a212a801d7c5a544dbc6a2babca8d
Agung Malik Al Qindy <agungalqindy@gmail.com>
2023-05-16 17:17:11 +0700

    add new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,38 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    daisyui: {
+        themes: [
+          {
+            mytheme: {
+
+                "primary": "#063777",
+
+                "secondary": "#3F87E5",
+
+                "text": "#071D53",
+
+                "neutral": "#191D24",
+
+                "base-100": "#2A303C",
+
+                "info": "#3ABFF8",
+
+                "success": "#36D399",
+
+                "warning": "#FBBD23",
+
+                "error": "#F87272",
+            },
+          },
+        ],
+    },
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -16,5 +44,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("daisyui"),
+        require('flowbite/plugin'),
+    ],
 };
