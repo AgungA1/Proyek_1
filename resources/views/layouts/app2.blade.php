4e47f47a6f94f8f70f3ede0490b4987412d13919
aldinpramudya <aldinarielpramudya@gmail.com>
2023-05-17 15:34:35 +0700

    Add view Navbar, Sidebar

---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tes</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    @yield('content')
+    <!-- Wrapper -->
+    <div class="p-4 sm:ml-64"> 
+        <!-- Content Warapper -->
+        <div class="p-4 mt-14">
+            
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/nav', function(){
+    return view('layouts.navbar');
+});
+
 require __DIR__.'/auth.php';
